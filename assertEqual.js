c0cb4d4cabff6c5ce7cc69d0fc6f7e95f6353942
Jeffrey <jeffrey.liu90@gmail.com>
2020-02-03 21:38:57 +0000

    Assert Equal Assignment

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👌👌👌 Assertion Passed: " + actual + " " + "=== " + expected);
+  } else {
+    console.log("🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍♂️ Assertion Failed: " + actual + " " + "!== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
