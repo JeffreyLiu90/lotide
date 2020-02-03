8f1d4685a433534d3918067bcc4030f8807074a5
Jeffrey <jeffrey.liu90@gmail.com>
2020-02-03 22:06:17 +0000

    assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("👌👌👌 Assertion Passed: " + actual + " " + "=== " + expected);
+    console.log(`👌👌👌 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍♂️ Assertion Failed: " + actual + " " + "!== " + expected);
+    console.log(`🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍♂️ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
