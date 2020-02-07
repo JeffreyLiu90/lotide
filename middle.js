147b13585acb354b71aee38334da30960f389d3b
Jeffrey <jeffrey.liu90@gmail.com>
2020-02-07 02:27:33 +0000

    middle

--- middle.js
+++ middle.js
@@ -1,28 +1,23 @@
-const assertEqual = function(actual, expected) {
+const assertArraysEqual = function(actual, expected) {
 
-  if (assertArraysEqual(actual, expected)) {
-    if (actual.length > 0 && actual.length <= 2) {
-    console.log(`👌👌👌 Assertion Passed: ${actual} === ${expected}, and the middle has no value because it is too short`);
-  } else {
-    console.log(`👌👌👌 Assertion Passed: ${actual} === ${expected}, and the middle is ${middle(actual)}`);
-  } } else {
-  console.log(`🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍♂️ Assertion Failed: ${actual} !== ${expected} and the middle of first array is ${middle(actual)} and middle of second array is ${middle(expected)}`);
-  }
-};
+  if (eqArrays(actual, expected)) {
+    console.log(`👌👌👌 Assertion Passed: ${actual} === ${expected}`)
+  }  else {
+  console.log(`🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍♂️ Assertion Failed: ${actual} !== ${expected}`)
+}
+}
 
-
-let assertArraysEqual = function(arr, arrTwo) {
+let eqArrays = function(arr, arrTwo) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] !== arrTwo[i]) {
       return false;
-    }
+    };
   }
   return true;
 };
 
-
 const middle = function(array) {
-  let output = "";
+  let output = [];
   if(array.length % 2 === 0) {
     output = [array[(array.length/2- 1)], array[(array.length/2)]]
     return output
@@ -33,4 +28,5 @@ return output
  
 }
 
-assertEqual(["1", "2", "3", "4", "5"], ["1", "2", "3", "4"]);
+
+assertArraysEqual(middle(["1", "2", "3", "4", "5", "6"]), ["3", "4"]);
