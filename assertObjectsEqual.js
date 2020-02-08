a45f043e4ff062c22ea7c6c926ce2b13012d7ca0
Jeffrey <jeffrey.liu90@gmail.com>
2020-02-08 22:47:44 +0000

    assertObjects Equal

---
+++ assertObjectsEqual.js
@@ -0,0 +1,48 @@
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect
+  if (eqObjects(actual,expected)) {
+    console.log(`👌👌👌 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍♂️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let eqObjects = function(object1, object2) {
+  let objectOneKeys = Object.keys(object1);  //store the keys of object 1 in objectOneKeys
+  let objectTwoKeys = Object.keys(object2); // store the keys of object 2 in objectTwoKeys
+  if (objectOneKeys.length !== objectTwoKeys.length) { // if the length of the keys are not equal, automatically false
+    return false;
+  }
+  for (let key of objectOneKeys) { /*iterate through object keys and store that in a variable*/
+    if (Array.isArray(object1[key])) { /*checks the value of each of the keys to see if its an array. If it is an array it sends that key and the second objects corresponding key to the function eqArrays. */
+      return eqArrays(object1[key],object2[key]);
+    } else if (object1[key] !== object2[key]) {/*checks the value of the object and the index stored in the
+        variable against the value of second object and*/
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
+  
+
+let eqArrays = function(arr, arrTwo) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+const cd3 = { c: "1", d: ["2", 3] };
+
+
+assertObjectsEqual(ab, ba); // => true
