9b9753355ace2cc974510370caa51839317b2856
Jeffrey <jeffrey.liu90@gmail.com>
2020-02-10 22:14:51 +0000

    assert equal test using mocha and chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,11 @@
-const assertEqual = require('../assertEqual')
+const { assert } = require('chai');
 
 
 
+describe("#eqArrays", () => {
+  it("returns assertion pass when comparing 'hello and 'hello' ", () => {
+    assert.strictEqual("hello", "hello");
+  });
+});
 
-assertEqual("hello", "hello")
+
