3b9da77973ab5e0195538830ea82e427546180ef
Jeffrey <jeffrey.liu90@gmail.com>
2020-02-10 20:30:53 +0000

    Refactoring Tail using import and export

--- tail.js
+++ tail.js
@@ -1,24 +1,29 @@
-const assertEqual = function(actual, expected) {
-  if (check(actual,expected)) {
-    console.log(`👌👌👌 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍♂️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = require('./assertEqual')
+
+// const assertEqual = function(actual, expected) {
+//   if (check(actual,expected)) {
+//     console.log(`👌👌👌 Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍♂️ Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 let tail = function(arr) {
   let arrOne =  arr.slice(1,arr.length);
   return arrOne;
 };
-let check = function(arr, arrTwo) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i], arrTwo[i]);
-    if (arr[i] !== arrTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+// let check = function(arr, arrTwo) {
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i], arrTwo[i]);
+//     if (arr[i] !== arrTwo[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]);
+
+module.exports = tail 
