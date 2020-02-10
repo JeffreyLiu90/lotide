797b326b86c4e7df6c99c4501b65d4ee26a0d629
Jeffrey <jeffrey.liu90@gmail.com>
2020-02-10 21:10:15 +0000

    Using Mocha and Chai for tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,23 @@
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert
 const tail = require('../tail')
 
+describe("#tail", () => {
+  it("returns Lighthouse for ['Hello', 'Lighthouse']", () => {
+    let result = tail(["Hello", "Lighthouse"])
+    assert.strictEqual(result[0],"Lighthouse");
+  });
+
+
+//   it("returns '5' for ['5']", () => {
+//     assert.strictEqual(head(['5']), '6'); 
+//   });
+
+});
 
 
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
